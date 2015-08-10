f7cee6a1b9fc9f43d6ca85bcabf9db14ea58a661
Taiyu <taiyu.len@gmail.com>
2015-08-10 15:50:47 -0700

    fixed 'return NULL' in 'void continer_map(...)'

--- sway/container.c
+++ sway/container.c
@@ -3,16 +3,16 @@
 
 void container_map(swayc_t *container, void (*f)(swayc_t *view, void *data), void *data) {
 	if (!container->children) {
-		return NULL;
+		return;
 	}
 	int i;
 	for (i = 0; i < container->children->length; ++i) {
 		swayc_t *child = container->children->items[i];
 		f(child, data);
 
-		if(child->children) 
+		if (child->children) {
 			container_map(child, f, data);
+		}
 	}
-	return NULL;
 }
 
