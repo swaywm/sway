0f387483fd6b4ce40514578ab008bfabc84b015d
Taiyu <taiyu.len@gmail.com>
2015-08-12 20:59:43 -0700

    moving stuff around

--- sway/main.c
+++ sway/main.c
@@ -12,31 +12,6 @@ int main(int argc, char **argv) {
 	init_log(L_DEBUG); // TODO: Control this with command line arg
 	init_layout();
 
-	static struct wlc_interface interface = {
-		.output = {
-			.created = handle_output_created,
-			.destroyed = handle_output_destroyed,
-			.resolution = handle_output_resolution_change,
-			.focus = handle_output_focused
-		},
-		.view = {
-			.created = handle_view_created,
-			.destroyed = handle_view_destroyed,
-			.focus = handle_view_focus,
-			.request = {
-				.geometry = handle_view_geometry_request
-			}
-		},
-		.keyboard = {
-			.key = handle_key
-		},
-		.pointer = {
-			.motion = handle_pointer_motion,
-			.button = handle_pointer_button
-		}
-
-	};
-
 	setenv("WLC_DIM", "0", 0);
 	if (!wlc_init(&interface, argc, argv)) {
 		return 1;
