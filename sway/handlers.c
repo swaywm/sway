269d6ba4ea9d317b4dfae640acce4d5ff142ef84
taiyu <taiyu.len@gmail.com>
2015-08-19 13:27:06 -0700

    fixed

--- sway/handlers.c
+++ sway/handlers.c
@@ -319,7 +319,7 @@ static bool handle_key(wlc_handle view, uint32_t time, const struct wlc_modifier
 	sway_log(L_DEBUG, "modifier %x: state %d: key %d, sym: %d",
 			modifiers->mods, state, key, sym);
 
-	if ((modifiers->mods & config->floating_mod) && (dragging || resizing)) {
+	if (state == WLC_KEY_STATE_PRESSED && (dragging || resizing)) {
 		reset_floating(get_focused_view(&root_container));
 	}
 
