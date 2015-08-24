7c5b6f8c5285c1ddb8604875a48c7798367cf8ad
Luminarys <kizunanohikari@gmail.com>
2015-08-24 15:44:58 -0500

    Added in backtrace printing

--- include/log.h
+++ include/log.h
@@ -16,6 +16,7 @@ void sway_log(log_importance_t verbosity, const char* format, ...) __attribute__
 void sway_log_errno(log_importance_t verbosity, char* format, ...) __attribute__((format(printf,2,3)));
 void sway_abort(const char* format, ...) __attribute__((format(printf,1,2)));
 bool sway_assert(bool condition, const char* format, ...) __attribute__((format(printf,2,3)));
+void error_handler(int sig);
 
 void layout_log(const swayc_t *c, int depth);
 #endif
