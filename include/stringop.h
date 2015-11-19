d0af224e6da2505601b9c068912903b3d32e37c6
S. Christoffer Eliesen <christoffer@eliesen.no>
2015-11-19 11:52:58 +0100

    stringop: lenient_strcmp: Add.

--- include/stringop.h
+++ include/stringop.h
@@ -14,6 +14,9 @@ char *strip_whitespace(char *str);
 char *strip_comments(char *str);
 void strip_quotes(char *str);
 
+// strcmp that also handles null pointers.
+int lenient_strcmp(char *a, char *b);
+
 // Simply split a string with delims, free with `free_flat_list`
 list_t *split_string(const char *str, const char *delims);
 void free_flat_list(list_t *list);
