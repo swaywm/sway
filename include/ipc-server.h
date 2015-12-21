66c66c730436f325c6917b787b742af5e89102ab
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2015-12-21 15:28:34 +0100

    Implement `barconfig_update` IPC event

--- include/ipc-server.h
+++ include/ipc-server.h
@@ -2,6 +2,7 @@
 #define _SWAY_IPC_SERVER_H
 
 #include "container.h"
+#include "config.h"
 #include "ipc.h"
 
 void ipc_init(void);
@@ -9,6 +10,7 @@ void ipc_terminate(void);
 struct sockaddr_un *ipc_user_sockaddr(void);
 
 void ipc_event_workspace(swayc_t *old, swayc_t *new);
+void ipc_event_barconfig_update(struct bar_config *bar);
 const char *swayc_type_string(enum swayc_types type);
 
 #endif
