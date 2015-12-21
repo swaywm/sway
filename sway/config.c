fc71c6303ca10b59c8f64e752307b0dd26832b52
Drew DeVault <sir@cmpwn.com>
2015-12-21 05:06:54 -0500

    Fix default swaybar font

--- sway/config.c
+++ sway/config.c
@@ -693,7 +693,7 @@ struct bar_config *default_bar_config(void) {
 	bar->bindings = create_list();
 	bar->status_command = strdup("while :; do date +'%Y-%m-%d %l:%M:%S %p' && sleep 1; done");
 	bar->swaybar_command = NULL;
-	bar->font = strdup("monospace 10");
+	bar->font = strdup("pango:monospace 10");
 	bar->height = -1;
 	bar->workspace_buttons = true;
 	bar->separator_symbol = NULL;
