227d9b82e4475c4789f2e9b5ff5409df50271e19
progandy <code@progandy>
2016-01-08 18:29:06 +0100

    ipc-client: add functions for send without receive

--- include/ipc-client.h
+++ include/ipc-client.h
@@ -22,6 +22,11 @@ char *get_socketpath(void);
  */
 int ipc_open_socket(const char *socket_path);
 /**
+ * Issues a single IPC command without waiting for a response.
+ * Useful if events are sent on the same socket.
+ */
+void ipc_send_command(int socketfd, uint32_t type, const char *payload, uint32_t len);
+/**
  * Issues a single IPC command and returns the buffer. len will be updated with
  * the length of the buffer returned from sway.
  */
