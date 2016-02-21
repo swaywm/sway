b107fc6e5a467b526e7d67ef09ca3b4044ab4976
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2016-02-21 22:49:20 +0100

    Fix ipc event bit masks

--- include/ipc.h
+++ include/ipc.h
@@ -11,15 +11,15 @@ enum ipc_command_type {
 	IPC_GET_BAR_CONFIG = 6,
 	IPC_GET_VERSION = 7,
 	IPC_GET_INPUTS = 8,
-	// Events send from sway to clients. Events have the higest bit set.
-	IPC_EVENT_WORKSPACE = (1 << 31 | 0),
-	IPC_EVENT_OUTPUT = (1 << 31 | 1),
-	IPC_EVENT_MODE = (1 << 31 | 2),
-	IPC_EVENT_WINDOW = (1 << 31 | 3),
-	IPC_EVENT_BARCONFIG_UPDATE = (1 << 31 | 4),
-	IPC_EVENT_BINDING = (1 << 31 | 5),
-	IPC_EVENT_MODIFIER = (1 << 31 | 6),
-	IPC_EVENT_INPUT = (1 << 31 | 7),
+	// Events send from sway to clients. Events have the higest bits set.
+	IPC_EVENT_WORKSPACE = (1 << (31 - 0)),
+	IPC_EVENT_OUTPUT = (1 << (31 - 1)),
+	IPC_EVENT_MODE = (1 << (31 - 2)),
+	IPC_EVENT_WINDOW = (1 << (32 - 3)),
+	IPC_EVENT_BARCONFIG_UPDATE = (1 << (31 - 4)),
+	IPC_EVENT_BINDING = (1 << (31 - 5)),
+	IPC_EVENT_MODIFIER = (1 << (31 - 6)),
+	IPC_EVENT_INPUT = (1 << (31 - 7)),
 	IPC_SWAY_GET_PIXELS = 0x81
 };
 
