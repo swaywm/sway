b5f34cc27d2c2461196dde44f858c6488e4d2772
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2016-03-26 13:02:16 +0100

    Load correct config on reload

--- sway/commands.c
+++ sway/commands.c
@@ -1555,7 +1555,9 @@ static struct cmd_results *cmd_reload(int argc, char **argv) {
 	if ((error = checkarg(argc, "reload", EXPECTED_EQUAL_TO, 0))) {
 		return error;
 	}
-	if (!load_main_config(NULL, true)) return cmd_results_new(CMD_FAILURE, "reload", "Error(s) reloading config.");
+	if (!load_main_config(config->current_config, true)) {
+		return cmd_results_new(CMD_FAILURE, "reload", "Error(s) reloading config.");
+	}
 
 	load_swaybars();
 
