3b1c125e18a85f917b03d865d56ee3705b813c3b
Drew DeVault <sir@cmpwn.com>
2016-05-29 11:53:13 -0400

    Remove bar position left:right from docs
    And adds a warning about using them (currently these are not supported
    by swaybar).

--- sway/commands.c
+++ sway/commands.c
@@ -2849,8 +2849,10 @@ static struct cmd_results *bar_cmd_position(int argc, char **argv) {
 	} else if (strcasecmp("bottom", argv[0]) == 0) {
 		config->current_bar->position = DESKTOP_SHELL_PANEL_POSITION_BOTTOM;
 	} else if (strcasecmp("left", argv[0]) == 0) {
+		sway_log(L_INFO, "Warning: swaybar currently only supports top and bottom positioning. YMMV");
 		config->current_bar->position = DESKTOP_SHELL_PANEL_POSITION_LEFT;
 	} else if (strcasecmp("right", argv[0]) == 0) {
+		sway_log(L_INFO, "Warning: swaybar currently only supports top and bottom positioning. YMMV");
 		config->current_bar->position = DESKTOP_SHELL_PANEL_POSITION_RIGHT;
 	} else {
 		error = cmd_results_new(CMD_INVALID, "position", "Invalid value %s", argv[0]);
