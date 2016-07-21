095353d91db1467cb0366cfe68773d679d029370
Drew DeVault <sir@cmpwn.com>
2016-07-21 08:26:42 -0400

    Remove arrange_windows from post-render
    Fixes #741

--- sway/handlers.c
+++ sway/handlers.c
@@ -196,7 +196,6 @@ static void handle_output_destroyed(wlc_handle output) {
 
 static void handle_output_post_render(wlc_handle output) {
 	ipc_get_pixels(output);
-	arrange_windows(swayc_by_handle(output), -1, -1);
 }
 
 static void handle_view_pre_render(wlc_handle view) {
