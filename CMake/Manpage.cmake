cf4ea4c86b2023e3d1f9fa54b9e1dcf2fc27ef3d
Drew DeVault <sir@cmpwn.com>
2017-07-04 16:00:13 -0400

    Make asciidoc (and man pages) optional
    And remove it from the build

--- CMake/Manpage.cmake
+++ CMake/Manpage.cmake
@@ -1,31 +1,33 @@
-find_package(A2X REQUIRED)
+find_package(A2X)
 
-add_custom_target(man ALL)
+if (A2X_FOUND)
+    add_custom_target(man ALL)
 
-function(add_manpage name section)
-	add_custom_command(
-		OUTPUT ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
-		COMMAND ${A2X_COMMAND}
-				--no-xmllint
-				--doctype manpage
-				--format manpage
-				-D ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-				${CMAKE_CURRENT_SOURCE_DIR}/${name}.${section}.txt
-		DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${name}.${section}.txt
-		COMMENT Generating manpage for ${name}.${section}
-	)
+    function(add_manpage name section)
+        add_custom_command(
+            OUTPUT ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
+            COMMAND ${A2X_COMMAND}
+                    --no-xmllint
+                    --doctype manpage
+                    --format manpage
+                    -D ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+                    ${CMAKE_CURRENT_SOURCE_DIR}/${name}.${section}.txt
+            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${name}.${section}.txt
+            COMMENT Generating manpage for ${name}.${section}
+        )
 
-	add_custom_target(man-${name}.${section}
-		DEPENDS
-			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
-	)
-	add_dependencies(man
-		man-${name}.${section}
-	)
+        add_custom_target(man-${name}.${section}
+            DEPENDS
+                ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
+        )
+        add_dependencies(man
+            man-${name}.${section}
+        )
 
-	install(
-		FILES ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
-		DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/man/man${section}
-		COMPONENT documentation
-	)
-endfunction()
+        install(
+            FILES ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${name}.${section}
+            DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/man/man${section}
+            COMPONENT documentation
+        )
+    endfunction()
+endif()
