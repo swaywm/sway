c3fc0d446f64ef7f46bc2a152cd68331de4410d0
Tony Crisci <tony@dubstepdish.com>
2018-01-21 14:15:10 -0500

    cmd-kill: use sway_assert when no container

--- sway/commands/kill.c
+++ sway/commands/kill.c
@@ -1,12 +1,13 @@
 #include <wlr/util/log.h>
+#include "log.h"
 #include "sway/input/input-manager.h"
 #include "sway/input/seat.h"
 #include "sway/view.h"
 #include "sway/commands.h"
 
 struct cmd_results *cmd_kill(int argc, char **argv) {
-	if (!config->handler_context.current_container) {
-		wlr_log(L_ERROR, "cmd_kill called without container context");
+	if (!sway_assert(config->handler_context.current_container,
+				"cmd_kill called without container context")) {
 		return cmd_results_new(CMD_INVALID, NULL,
 				"cmd_kill called without container context "
 				"(this is a bug in sway)");
