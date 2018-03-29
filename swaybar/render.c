3a458cd7b55bdfad1b04a01064f4fe8fa86ed0de
Drew DeVault <sir@cmpwn.com>
2018-03-29 00:49:59 -0400

    Implement workspace button rendering

--- swaybar/render.c
+++ swaybar/render.c
@@ -10,8 +10,79 @@
 #include "swaybar/render.h"
 #include "wlr-layer-shell-unstable-v1-client-protocol.h"
 
-static uint32_t render_to_cairo(cairo_t *cairo, struct swaybar *bar,
-		struct swaybar_output *output) {
+static const char *strip_workspace_number(const char *ws_name) {
+	size_t len = strlen(ws_name);
+	for (size_t i = 0; i < len; ++i) {
+		if (ws_name[i] < '0' || ws_name[i] > '9') {
+			if (':' == ws_name[i] && i < len - 1 && i > 0) {
+				return ws_name + i + 1;
+			}
+			return ws_name;
+		}
+	}
+	return ws_name;
+}
+
+static uint32_t render_workspace_button(cairo_t *cairo,
+		struct swaybar_config *config, struct swaybar_workspace *ws,
+		double *x, uint32_t height) {
+	static const int ws_horizontal_padding = 5;
+	static const double ws_vertical_padding = 1.5;
+	static const int ws_spacing = 1;
+
+	const char *name = ws->name;
+	if (config->strip_workspace_numbers) {
+		name = strip_workspace_number(ws->name);
+	}
+
+	struct box_colors box_colors;
+	if (ws->urgent) {
+		box_colors = config->colors.urgent_workspace;
+	} else if (ws->focused) {
+		box_colors = config->colors.focused_workspace;
+	} else if (ws->visible) {
+		box_colors = config->colors.active_workspace;
+	} else {
+		box_colors = config->colors.inactive_workspace;
+	}
+
+	int text_width, text_height;
+	get_text_size(cairo, config->font, &text_width, &text_height,
+			1, true, "%s", name);
+	uint32_t ideal_height = ws_vertical_padding * 2 + text_height;
+	if (height < ideal_height) {
+		height = ideal_height;
+	}
+	uint32_t width = ws_horizontal_padding * 2 + text_width;
+
+	cairo_set_source_u32(cairo, box_colors.background);
+	cairo_rectangle(cairo, *x, 0, width - 1, height);
+	cairo_fill(cairo);
+
+	cairo_set_source_u32(cairo, box_colors.border);
+	cairo_rectangle(cairo, *x, 0, width - 1, height);
+	cairo_stroke(cairo);
+
+	double text_y = height / 2.0 - text_height / 2.0;
+	cairo_set_source_u32(cairo, box_colors.text);
+	cairo_move_to(cairo, (int)*x + ws_horizontal_padding, (int)floor(text_y));
+	pango_printf(cairo, config->font, 1, true, "%s", name);
+
+	*x += width + ws_spacing;
+	return ideal_height;
+}
+
+static void update_heights(uint32_t height, uint32_t *min, uint32_t *max) {
+	if (*min < height) {
+		*min = height;
+	}
+	if (height > *max) {
+		*max = height;
+	}
+}
+
+static uint32_t render_to_cairo(cairo_t *cairo,
+		struct swaybar *bar, struct swaybar_output *output) {
 	struct swaybar_config *config = bar->config;
 
 	cairo_save(cairo);
@@ -27,8 +98,20 @@ static uint32_t render_to_cairo(cairo_t *cairo, struct swaybar *bar,
 	}
 	cairo_paint(cairo);
 
-	// TODO: use actual height
-	return 20;
+	uint32_t min_height = output->height, max_height = output->height;
+
+	double x = 0;
+	if (config->workspace_buttons) {
+		struct swaybar_workspace *ws;
+		wl_list_for_each(ws, &output->workspaces, link) {
+			uint32_t h = render_workspace_button(
+					cairo, config, ws, &x, output->height);
+			update_heights(h, &min_height, &max_height);
+		}
+	}
+
+	// TODO: Shrink via min_height if sane
+	return max_height;
 }
 
 void render_frame(struct swaybar *bar,
@@ -41,6 +124,7 @@ void render_frame(struct swaybar *bar,
 		// Reconfigure surface
 		zwlr_layer_surface_v1_set_size(
 				output->layer_surface, 0, height);
+		zwlr_layer_surface_v1_set_exclusive_zone(output->layer_surface, height);
 		// TODO: this could infinite loop if the compositor assigns us a
 		// different height than what we asked for
 		wl_surface_commit(output->surface);
