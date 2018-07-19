4154234eee1fa86f3a01d2f47b4fa632dcd73644
emersion <contact@emersion.fr>
2018-07-19 21:54:46 +0100

    Update for swaywm/wlroots#1148

--- sway/server.c
+++ sway/server.c
@@ -11,7 +11,7 @@
 #include <wlr/types/wlr_gamma_control.h>
 #include <wlr/types/wlr_idle.h>
 #include <wlr/types/wlr_layer_shell.h>
-#include <wlr/types/wlr_linux_dmabuf.h>
+#include <wlr/types/wlr_linux_dmabuf_v1.h>
 #include <wlr/types/wlr_primary_selection.h>
 #include <wlr/types/wlr_screencopy_v1.h>
 #include <wlr/types/wlr_server_decoration.h>
@@ -108,7 +108,7 @@ bool server_init(struct sway_server *server) {
 	wlr_server_decoration_manager_set_default_mode(
 		deco_manager, WLR_SERVER_DECORATION_MANAGER_MODE_SERVER);
 
-	wlr_linux_dmabuf_create(server->wl_display, renderer);
+	wlr_linux_dmabuf_v1_create(server->wl_display, renderer);
 	wlr_export_dmabuf_manager_v1_create(server->wl_display);
 	wlr_screencopy_manager_v1_create(server->wl_display);
 
