238c8afc74241efdc44d1cf88322d322ce1226d9
emersion <contact@emersion.fr>
2018-07-22 22:20:07 +0100

    Handle set_{title,app_id} for xdg-shell and zxdg-shell-v6
    This allows to update the title even if the view doesn't commit.
    This is useful e.g. when a terminal sets its toplevel title to
    the currently running command and when the view isn't visible.

--- include/sway/tree/view.h
+++ include/sway/tree/view.h
@@ -110,6 +110,8 @@ struct sway_xdg_shell_v6_view {
 	struct wl_listener request_resize;
 	struct wl_listener request_maximize;
 	struct wl_listener request_fullscreen;
+	struct wl_listener set_title;
+	struct wl_listener set_app_id;
 	struct wl_listener new_popup;
 	struct wl_listener map;
 	struct wl_listener unmap;
@@ -124,6 +126,8 @@ struct sway_xdg_shell_view {
 	struct wl_listener request_resize;
 	struct wl_listener request_maximize;
 	struct wl_listener request_fullscreen;
+	struct wl_listener set_title;
+	struct wl_listener set_app_id;
 	struct wl_listener new_popup;
 	struct wl_listener map;
 	struct wl_listener unmap;
