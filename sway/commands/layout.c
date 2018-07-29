be64c466245d4c537a70484abfe21adc9d60f3c5
Ian Fan <ianfan0@gmail.com>
2018-07-29 12:27:34 +0100

    commands: complete layout implementation

--- sway/commands/layout.c
+++ sway/commands/layout.c
@@ -1,9 +1,30 @@
+#include <stdbool.h>
 #include <string.h>
 #include <strings.h>
 #include "sway/commands.h"
 #include "sway/tree/arrange.h"
 #include "sway/tree/container.h"
 #include "log.h"
+
+static bool parse_layout_string(char *s, enum sway_container_layout *ptr) {
+	if (strcasecmp(s, "splith") == 0) {
+		*ptr = L_HORIZ;
+	} else if (strcasecmp(s, "splitv") == 0) {
+		*ptr = L_VERT;
+	} else if (strcasecmp(s, "tabbed") == 0) {
+		*ptr = L_TABBED;
+	} else if (strcasecmp(s, "stacking") == 0) {
+		*ptr = L_STACKED;
+	} else {
+		return false;
+	}
+	return true;
+}
+
+static const char* expected_syntax =
+	"Expected 'layout default|tabbed|stacking|splitv|splith' or "
+	"'layout toggle [split|all]' or "
+	"'layout toggle [split|tabbed|stacking|splitv|splith] [split|tabbed|stacking|splitv|splith]...'";
 
 struct cmd_results *cmd_layout(int argc, char **argv) {
 	struct cmd_results *error = NULL;
@@ -23,29 +44,54 @@ struct cmd_results *cmd_layout(int argc, char **argv) {
 
 	if (strcasecmp(argv[0], "default") == 0) {
 		parent->layout = parent->prev_layout;
-		if (parent->layout == L_NONE) {
-			parent->layout = container_get_default_layout(parent);
-		}
 	} else {
 		if (parent->layout != L_TABBED && parent->layout != L_STACKED) {
 			parent->prev_layout = parent->layout;
 		}
 
-		if (strcasecmp(argv[0], "splith") == 0) {
-			parent->layout = L_HORIZ;
-		} else if (strcasecmp(argv[0], "splitv") == 0) {
-			parent->layout = L_VERT;
-		} else if (strcasecmp(argv[0], "tabbed") == 0) {
-			parent->layout = L_TABBED;
-		} else if (strcasecmp(argv[0], "stacking") == 0) {
-			parent->layout = L_STACKED;
-		} else if (strcasecmp(argv[0], "toggle") == 0 && argc == 2 && strcasecmp(argv[1], "split") == 0) {
-			if (parent->layout == L_HORIZ) {
-				parent->layout = L_VERT;
+		bool assigned_directly = parse_layout_string(argv[0], &parent->layout);
+		if (!assigned_directly && strcasecmp(argv[0], "toggle") == 0) {
+			if (argc == 1) {
+				parent->layout =
+					parent->layout == L_STACKED ? L_TABBED :
+					parent->layout == L_TABBED ? parent->prev_layout : L_STACKED;
+			} else if (argc == 2) {
+				if (strcasecmp(argv[1], "all") == 0) {
+					parent->layout =
+						parent->layout == L_HORIZ ? L_VERT :
+						parent->layout == L_VERT ? L_STACKED :
+						parent->layout == L_STACKED ? L_TABBED : L_HORIZ;
+				} else if (strcasecmp(argv[1], "split") == 0) {
+					parent->layout = parent->layout == L_VERT ? L_HORIZ : L_VERT;
+				} else {
+					return cmd_results_new(CMD_INVALID, "layout", expected_syntax);
+				}
 			} else {
-				parent->layout = L_HORIZ;
+				bool valid;
+				enum sway_container_layout parsed_layout;
+				int curr = 1;
+				for (; curr < argc; curr++) {
+					valid = parse_layout_string(argv[curr], &parsed_layout);
+					if (valid && parsed_layout == parent->layout) {
+						break;
+					}
+				}
+				for (int i = curr + 1; i != curr; ++i) {
+					// cycle round to find next valid layout
+					if (i >= argc) {
+						i = 1;
+					}
+					if (parse_layout_string(argv[i], &parent->layout)) {
+						break;
+					} // invalid layout strings are silently ignored
+				}
 			}
+		} else {
+			return cmd_results_new(CMD_INVALID, "layout", expected_syntax);
 		}
+	}
+	if (parent->layout == L_NONE) {
+		parent->layout = container_get_default_layout(parent);
 	}
 
 	container_notify_subtree_changed(parent);
