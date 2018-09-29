8f09ba78859ea894e843258246156406566d8e51
Brian Ashworth <bosrsf04@gmail.com>
2018-09-28 23:47:22 -0400

    Move sticky containers in output_evacuate

--- sway/tree/container.c
+++ sway/tree/container.c
@@ -823,9 +823,16 @@ void container_floating_move_to_center(struct sway_container *con) {
 		return;
 	}
 	struct sway_workspace *ws = con->workspace;
+	bool full = con->is_fullscreen;
+	if (full) {
+		container_set_fullscreen(con, false);
+	}
 	double new_lx = ws->x + (ws->width - con->width) / 2;
 	double new_ly = ws->y + (ws->height - con->height) / 2;
 	container_floating_translate(con, new_lx - con->x, new_ly - con->y);
+	if (full) {
+		container_set_fullscreen(con, true);
+	}
 }
 
 static bool find_urgent_iterator(struct sway_container *con, void *data) {
