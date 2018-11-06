c5bf41ec97579464780ce66a05ee71e7a01261d3
Connor E <38229097+c-edw@users.noreply.github.com>
2018-11-06 17:24:56 +0000

    Fail if focus_follows_mouse is invalid.

--- sway/commands/focus_follows_mouse.c
+++ sway/commands/focus_follows_mouse.c
@@ -7,13 +7,15 @@ struct cmd_results *cmd_focus_follows_mouse(int argc, char **argv) {
 	struct cmd_results *error = NULL;
 	if ((error = checkarg(argc, "focus_follows_mouse", EXPECTED_EQUAL_TO, 1))) {
 		return error;
-	}
-	if(strcmp(argv[0], "no") == 0) {
+	} else if(strcmp(argv[0], "no") == 0) {
 		config->focus_follows_mouse = FOLLOWS_NO;
 	} else if(strcmp(argv[0], "yes") == 0) {
 		config->focus_follows_mouse = FOLLOWS_YES;
 	} else if(strcmp(argv[0], "always") == 0) {
 		config->focus_follows_mouse = FOLLOWS_ALWAYS;
+	} else {
+		return cmd_results_new(CMD_FAILURE, "focus_follows_mouse",
+				"Expected 'focus_follows_mouse no|yes|always'");
 	}
 	return cmd_results_new(CMD_SUCCESS, NULL, NULL);
 }
