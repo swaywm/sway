a20f9829ba0b94ea72e8adfea640ab0e6189d022
Ryan Dwyer <ryandwyer1@gmail.com>
2019-01-20 10:50:11 +1000

    Prevent noop output from being enabled

--- sway/commands/output/enable.c
+++ sway/commands/output/enable.c
@@ -1,3 +1,4 @@
+#include <strings.h>
 #include "sway/commands.h"
 #include "sway/config.h"
 
@@ -5,6 +6,15 @@ struct cmd_results *output_cmd_enable(int argc, char **argv) {
 	if (!config->handler_context.output_config) {
 		return cmd_results_new(CMD_FAILURE, "Missing output config");
 	}
+
+	// The NOOP-1 output is a dummy output used when there's no outputs
+	// connected. It should never be enabled.
+	char *output_name = config->handler_context.output_config->name;
+	if (strcasecmp(output_name, "NOOP-1") == 0) {
+		return cmd_results_new(CMD_FAILURE,
+				"Refusing to enable the no op output");
+	}
+
 	config->handler_context.output_config->enabled = 1;
 
 	config->handler_context.leftovers.argc = argc;
