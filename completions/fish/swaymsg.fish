b7fe5097e949b9bfde6fd52f26d2599068833563
Peter Grayson <pete@jpgrayson.net>
2019-03-08 12:43:04 -0500

    Add -p/--pretty option to swaymsg
    This new option forces pretty (non-raw/non-JSON) output. By default, when
    not using a tty, swaymsg outputs using the "raw" format. This makes it
    impossible to, for example, pipe the pretty output to a pager such as
    `less` since piping does not use a tty.
    
    The new -p/--pretty option gives the user explicit control over the output
    format while retaining the default tty-dependent behavior.
    
    Signed-off-by: Peter Grayson <pete@jpgrayson.net>

--- completions/fish/swaymsg.fish
+++ completions/fish/swaymsg.fish
@@ -2,10 +2,11 @@
 
 complete -f -c swaymsg
 complete -c swaymsg -s h -l help --description "Show help message and quit."
+complete -c swaymsg -s p -l pretty --description "Use pretty output even when not using a tty."
 complete -c swaymsg -s q -l quiet --description "Sends the IPC message but does not print the response from sway."
-complete -c swaymsg -s v -l version --description "Print the version (of swaymsg) and quit."
 complete -c swaymsg -s r -l raw --description "Use raw output even if using tty."
 complete -c swaymsg -s s -l socket -r --description "Use the specified socket path. Otherwise, swaymsg will ask where the socket is (which is the value of $SWAYSOCK, then of $I3SOCK)."
+complete -c swaymsg -s v -l version --description "Print the version (of swaymsg) and quit."
 
 complete -c swaymsg -s t -l type -fr --description "Specify the type of IPC message."
 complete -c swaymsg -s t -l type -fra 'get_workspaces' --description "Gets a JSON-encoded list of workspaces and their status."
