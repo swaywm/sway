b2cfcebef61087f819b55d2f815e62aa4a6f1a52
Connor E <38229097+c-edw@users.noreply.github.com>
2019-04-15 01:16:35 +0100

    Add deprecation warnings for new_float, new_window, and force_focus_wrapping.

--- sway/commands/force_focus_wrapping.c
+++ sway/commands/force_focus_wrapping.c
@@ -1,9 +1,16 @@
-#include <strings.h>
 #include "sway/commands.h"
 #include "sway/config.h"
+#include "log.h"
 #include "util.h"
 
 struct cmd_results *cmd_force_focus_wrapping(int argc, char **argv) {
+	sway_log(SWAY_INFO, "Warning: force_focus_wrapping is deprecated. "
+		"Use focus_wrapping instead.");
+	if (config->reading) {
+		config_add_swaynag_warning("force_focus_wrapping is deprecated. "
+			"Use focus_wrapping instead.");
+	}
+
 	struct cmd_results *error =
 		checkarg(argc, "force_focus_wrapping", EXPECTED_EQUAL_TO, 1);
 	if (error) {
