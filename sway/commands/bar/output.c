e12b3667a94ce78adb974cb4f94d0ffbe58c4290
Alyssa Ross <hi@alyssa.is>
2019-05-16 22:56:58 +0000

    bar: fix segfault with missing or invalid bar id
    Prior to this patch, if I ran something like this, sway would crash:
    
        swaymsg bar height 50
    
    or
    
        swaymsg bar not-a-bar-id color bg #ff0000
    
    This was in contrast to other bar subcommands, like status_command,
    which would exit with a "No bar defined" message.
    
    The difference between the subcommands that crashed and the ones that
    exited was that some subcommands had a check to see if a bar was
    specified, while others just assumed that it had been and carried on
    until they segfaulted.
    
    Because this check was identical in every subcommand it was present in,
    and I couldn't think of a case where it would be valid to run a bar
    subcommand without specifying which bar to apply it to, I moved this
    check from individual subcommands into the bar command, which is already
    responsible for actually setting the specified bar. This reduced code
    duplication, and fixed the crash for the subcommands that were missing
    this check.

--- sway/commands/bar/output.c
+++ sway/commands/bar/output.c
@@ -10,9 +10,6 @@ struct cmd_results *bar_cmd_output(int argc, char **argv) {
 	if ((error = checkarg(argc, "output", EXPECTED_EQUAL_TO, 1))) {
 		return error;
 	}
-	if (!config->current_bar) {
-		return cmd_results_new(CMD_FAILURE, "No bar defined.");
-	}
 
 	const char *output = argv[0];
 	list_t *outputs = config->current_bar->outputs;
