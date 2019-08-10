0570eb19e5cd3d6f4839f3b4fc921e9fbd1f0aa0
Antonin Décimo <antonin.decimo@gmail.com>
2019-08-10 16:47:19 +0200

    view_update_size: fix surface_width/height mismatch

--- sway/tree/view.c
+++ sway/tree/view.c
@@ -709,7 +709,7 @@ void view_update_size(struct sway_view *view, int width, int height) {
 		con->surface_y = fmax(con->surface_y, con->content_y);
 	}
 	con->surface_width = width;
-	con->surface_width = height;
+	con->surface_height = height;
 }
 
 static const struct sway_view_child_impl subsurface_impl;
