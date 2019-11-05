swaywm/sway

commit f33dcd4c604fa24058f6cbaf692831420dc5db67

Prefer higher refresh rate default modes

sway/config/output.c
@@ -222,7 +222,7 @@ static bool set_mode(struct wlr_output *output, int width, int height,
 				best = mode;
 				break;
 			}
-			best = mode;
+			best = mode->refresh > best->refresh ? mode : best;
 		}
 	}
 	if (!best) {

commit 6f5d2efa0fb67e31b460587700e805b6f0020139

Fix segfault in set_mode
best is NULL prior to being assigned to a mode.

Closes: https://github.com/swaywm/sway/issues/4705
Fixes: f33dcd4c604f ("Prefer higher refresh rate default modes")

sway/config/output.c
@@ -222,7 +222,9 @@ static bool set_mode(struct wlr_output *output, int width, int height,
 				best = mode;
 				break;
 			}
-			best = mode->refresh > best->refresh ? mode : best;
+			if (best == NULL || mode->refresh > best->refresh) {
+				best = mode;
+			}
 		}
 	}
 	if (!best) {