swaywm/sway

commit ab0248a54564b2f644b6fb367f9eb44fe0bf5f3c

seat_create: set initial focus for added seats
This sets the initial focus for all seats other than the first seat,
which gets it focus on launch. The ensures that all seats have something
focused.

sway/input/seat.c
@@ -357,6 +357,14 @@ struct sway_seat *seat_create(const char *seat_name) {
 	root_for_each_workspace(collect_focus_workspace_iter, seat);
 	root_for_each_container(collect_focus_container_iter, seat);
 
+	if (!wl_list_empty(&server.input->seats)) {
+		// Since this is not the first seat, attempt to set initial focus
+		struct sway_seat *current_seat = input_manager_current_seat();
+		struct sway_node *current_focus =
+			seat_get_focus_inactive(current_seat, &root->node);
+		seat_set_focus(seat, current_focus);
+	}
+
 	wl_signal_add(&root->events.new_node, &seat->new_node);
 	seat->new_node.notify = handle_new_node;
 

commit 45990be340fe594bff997d417c2d7010298d03a6

input: seat: Fix seat device list not initialised before use
When being created, non first seats would get through the list of devices
without the list being first initialised -> segfault.
Issue introduced with ab0248a54564b2f644b6fb367f9eb44fe0bf5f3c

Fixes #4750: Crash when reloading Sway with multiple seats configured

sway/input/seat.c
@@ -494,6 +494,8 @@ struct sway_seat *seat_create(const char *seat_name) {
 	// init the focus stack
 	wl_list_init(&seat->focus_stack);
 
+	wl_list_init(&seat->devices);
+
 	root_for_each_workspace(collect_focus_workspace_iter, seat);
 	root_for_each_container(collect_focus_container_iter, seat);
 
@@ -526,7 +528,6 @@ struct sway_seat *seat_create(const char *seat_name) {
 	seat->request_set_primary_selection.notify =
 		handle_request_set_primary_selection;
 
-	wl_list_init(&seat->devices);
 	wl_list_init(&seat->keyboard_groups);
 
 	wl_list_insert(&server.input->seats, &seat->link);