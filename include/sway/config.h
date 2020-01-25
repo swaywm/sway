7cfa1507881144445efd9752e18219dd5ec4a14b
Michael Weiser <michael.weiser@gmx.de>
2020-01-25 19:27:03 +0100

    config: Remove unused mouse binding structure
    Mouse bindings are handled alongside normal bindings. Remove the unused
    separate data structure definition to avoid confusion.
    
    Signed-off-by: Michael Weiser <michael.weiser@gmx.de>

--- include/sway/config.h
+++ include/sway/config.h
@@ -48,7 +48,7 @@ enum binding_flags {
 };
 
 /**
- * A key binding and an associated command.
+ * A key (or mouse) binding and an associated command.
  */
 struct sway_binding {
 	enum binding_input_type type;
@@ -59,14 +59,6 @@ struct sway_binding {
 	list_t *syms; // sorted in ascending order; NULL if BINDING_CODE is not set
 	uint32_t modifiers;
 	xkb_layout_index_t group;
-	char *command;
-};
-
-/**
- * A mouse binding and an associated command.
- */
-struct sway_mouse_binding {
-	uint32_t button;
 	char *command;
 };
 
