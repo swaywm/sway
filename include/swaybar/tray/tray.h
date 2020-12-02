fdbe98512a71524091375290ca2c1c9450e4db84
Simon Ser <contact@emersion.fr>
2020-12-02 23:46:49 +0100

    build: introduce sd-bus-provider option
    This allows to select a specific provider for the sd-bus library.

--- include/swaybar/tray/tray.h
+++ include/swaybar/tray/tray.h
@@ -2,9 +2,9 @@
 #define _SWAYBAR_TRAY_TRAY_H
 
 #include "config.h"
-#ifdef HAVE_SYSTEMD
+#ifdef HAVE_LIBSYSTEMD
 #include <systemd/sd-bus.h>
-#elif HAVE_ELOGIND
+#elif HAVE_LIBELOGIND
 #include <elogind/sd-bus.h>
 #endif
 #include <cairo.h>
