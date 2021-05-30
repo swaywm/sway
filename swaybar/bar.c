2f304ef0532a45d00b2ec2c7fc63adef0aec7607
Felix Weilbach <felix.weilbach@t-online.de>
2021-05-30 20:45:01 +0200

    Tray: Implement dbusmenu
    Co-authored-by: Ian Fan <ianfan0@gmail.com>
    Co-authored-by: Nathan Schulte <nmschulte@gmail.com>
    
    Signed-off-by: Felix Weilbach <felix.weilbach@t-online.de>

--- swaybar/bar.c
+++ swaybar/bar.c
@@ -29,6 +29,7 @@
 #include "pool-buffer.h"
 #include "wlr-layer-shell-unstable-v1-client-protocol.h"
 #include "xdg-output-unstable-v1-client-protocol.h"
+#include "xdg-shell-client-protocol.h"
 
 void free_workspaces(struct wl_list *list) {
 	struct swaybar_workspace *ws, *tmp;
@@ -362,6 +363,8 @@ static void handle_global(void *data, struct wl_registry *registry,
 	} else if (strcmp(interface, zxdg_output_manager_v1_interface.name) == 0) {
 		bar->xdg_output_manager = wl_registry_bind(registry, name,
 			&zxdg_output_manager_v1_interface, 2);
+	} else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
+		bar->wm_base = wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
 	}
 }
 
@@ -534,6 +537,7 @@ void bar_teardown(struct swaybar *bar) {
 #if HAVE_TRAY
 	destroy_tray(bar->tray);
 #endif
+	xdg_wm_base_destroy(bar->wm_base);
 	free_outputs(&bar->outputs);
 	free_outputs(&bar->unused_outputs);
 	free_seats(&bar->seats);
