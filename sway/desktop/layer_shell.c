swaywm/sway

commit c11b5db4d6c1b9ce658405d92a23dd2993efca8e

layer-shell: check `committed` bitmask
This avoids infinite configure-ack_configure-commit loop.

sway/desktop/layer_shell.c
@@ -297,6 +297,10 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 	if (wlr_output == NULL) {
 		return;
 	}
+	if (layer_surface->current.committed == 0) {
+		// The layer surface state didn't change
+		return;
+	}
 
 	struct sway_output *output = wlr_output->data;
 	struct wlr_box old_extent = layer->extent;

commit fe4942e078f04fdc1958591bf2c92e9d3356e6b2

Fix layer surface commit handler
Fixes: c11b5db4d6c1 ("layer-shell: check `committed` bitmask")

sway/desktop/layer_shell.c
@@ -297,18 +297,19 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 	if (wlr_output == NULL) {
 		return;
 	}
-	if (layer_surface->current.committed == 0) {
-		// The layer surface state didn't change
-		return;
-	}
 
 	struct sway_output *output = wlr_output->data;
 	struct wlr_box old_extent = layer->extent;
-	arrange_layers(output);
+	bool layer_changed =
+		layer_surface->current.committed & WLR_LAYER_SURFACE_V1_STATE_LAYER;
+	bool extent_changed = false;
+	if (layer_surface->current.committed != 0) {
+		arrange_layers(output);
 
-	bool extent_changed =
-		memcmp(&old_extent, &layer->extent, sizeof(struct wlr_box)) != 0;
-	bool layer_changed = layer->layer != layer_surface->current.layer;
+		extent_changed = memcmp(&old_extent, &layer->extent,
+			sizeof(struct wlr_box)) != 0;
+	}
+
 	if (layer_changed) {
 		wl_list_remove(&layer->link);
 		wl_list_insert(&output->layers[layer_surface->current.layer],