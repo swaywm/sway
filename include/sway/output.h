fdc2053bfe4874c4f7da98d17a07657880079a17
Alexander Orzechowski <orzechowski.alexander@gmail.com>
2022-02-21 15:21:52 -0500

    renderer: Remove in favor of scene_graph

--- include/sway/output.h
+++ include/sway/output.h
@@ -177,12 +177,6 @@ void output_get_box(struct sway_output *output, struct wlr_box *box);
 enum sway_container_layout output_get_default_layout(
 		struct sway_output *output);
 
-void render_rect(struct sway_output *output,
-		pixman_region32_t *output_damage, const struct wlr_box *_box,
-		float color[static 4]);
-
-void premultiply_alpha(float color[4], float opacity);
-
 void scale_box(struct wlr_box *box, float scale);
 
 enum wlr_direction opposite_direction(enum wlr_direction d);
