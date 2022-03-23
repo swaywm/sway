68b245419b6180e51e5b0cadf563c7289467feb4
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-03-23 09:13:52 -0600

    sway/input: wlr_seat_keyboard() now takes wlr_keyboard

--- sway/input/seat.c
+++ sway/input/seat.c
@@ -813,7 +813,7 @@ static void seat_configure_keyboard(struct sway_seat *seat,
 	}
 	sway_keyboard_configure(seat_device->keyboard);
 	wlr_seat_set_keyboard(seat->wlr_seat,
-			seat_device->input_device->wlr_device);
+			seat_device->input_device->wlr_device->keyboard);
 	struct sway_node *focus = seat_get_focus(seat);
 	if (focus && node_is_view(focus)) {
 		// force notify reenter to pick up the new configuration
