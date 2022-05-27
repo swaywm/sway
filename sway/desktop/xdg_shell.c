26a0e97634dc641337fbb25cc193f26177265d14
Kirill Primak <vyivel@eclair.cafe>
2022-05-27 16:37:35 +0300

    chore: chase wlroots xdg-shell update

--- sway/desktop/xdg_shell.c
+++ sway/desktop/xdg_shell.c
@@ -27,8 +27,8 @@ static void popup_get_view_coords(struct sway_view_child *child,
 	struct wlr_xdg_popup *wlr_popup = popup->wlr_xdg_popup;
 
 	wlr_xdg_popup_get_toplevel_coords(wlr_popup,
-		wlr_popup->geometry.x - wlr_popup->base->current.geometry.x,
-		wlr_popup->geometry.y - wlr_popup->base->current.geometry.y,
+		wlr_popup->current.geometry.x - wlr_popup->base->current.geometry.x,
+		wlr_popup->current.geometry.y - wlr_popup->base->current.geometry.y,
 		sx, sy);
 }
 
