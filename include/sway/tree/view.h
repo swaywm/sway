6a59e38aeb71ad55191a20d81f9367141673c96b
Kirill Primak <vyivel@eclair.cafe>
2022-05-27 16:42:47 +0300

    xdg-shell: schedule a configure on maximize request
    This commit reverts 03879290dbee26127f6867ef60bc2a7f9a6c8c5f and
    fc84bcb7fb0ffa29b1f9bed287762241a3473803.

--- include/sway/tree/view.h
+++ include/sway/tree/view.h
@@ -131,6 +131,7 @@ struct sway_xdg_shell_view {
 	struct wl_listener commit;
 	struct wl_listener request_move;
 	struct wl_listener request_resize;
+	struct wl_listener request_maximize;
 	struct wl_listener request_fullscreen;
 	struct wl_listener set_title;
 	struct wl_listener set_app_id;
