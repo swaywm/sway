445bc2a943d905a0e5b1dc607a6797919c8cf341
Simon Ser <contact@emersion.fr>
2022-06-22 22:06:21 +0200

    Rename dpms output command to power
    The "dpms" command refers to VESA Display Power Management
    Signaling, a deprecated standard. It's superseded by VESA DPM.
    
    Instead of tying out command name to a particular standard, use the
    neutral term "power".

--- include/sway/commands.h
+++ include/sway/commands.h
@@ -287,6 +287,7 @@ sway_cmd output_cmd_max_render_time;
 sway_cmd output_cmd_mode;
 sway_cmd output_cmd_modeline;
 sway_cmd output_cmd_position;
+sway_cmd output_cmd_power;
 sway_cmd output_cmd_render_bit_depth;
 sway_cmd output_cmd_scale;
 sway_cmd output_cmd_scale_filter;
