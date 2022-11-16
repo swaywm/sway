eb5021ef990fb29ff86544aea58d687ad62c757a
Ronan Pigott <ronan@rjp.ie>
2022-11-16 15:50:34 -0700

    root: move the workspace matching code to its own file
    This removes the pid_workspace bits from tree/root before it gets
    too interesting.
    
    No functional change.

---
+++ include/sway/desktop/launcher.h
@@ -0,0 +1,14 @@
+#ifndef _SWAY_LAUNCHER_H
+#define _SWAY_LAUNCHER_H
+
+#include <stdlib.h>
+
+struct sway_workspace *root_workspace_for_pid(pid_t pid);
+
+void root_record_workspace_pid(pid_t pid);
+
+void root_remove_workspace_pid(pid_t pid);
+
+void root_rename_pid_workspaces(const char *old_name, const char *new_name);
+
+#endif
