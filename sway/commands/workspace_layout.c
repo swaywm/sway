0c83a9564c8444c2611669f82119b7a8234860d1
Mann mit Hut <github+mmhat@with-h.at>
2022-12-21 18:14:26 +0100

    Added splith and splitv to workspace_layout

--- sway/commands/workspace_layout.c
+++ sway/commands/workspace_layout.c
@@ -9,13 +9,17 @@ struct cmd_results *cmd_workspace_layout(int argc, char **argv) {
 	}
 	if (strcasecmp(argv[0], "default") == 0) {
 		config->default_layout = L_NONE;
+	} else if (strcasecmp(argv[0], "splith") == 0) {
+		config->default_layout = L_HORIZ;
+	} else if (strcasecmp(argv[0], "splitv") == 0) {
+		config->default_layout = L_VERT;
 	} else if (strcasecmp(argv[0], "stacking") == 0) {
 		config->default_layout = L_STACKED;
 	} else if (strcasecmp(argv[0], "tabbed") == 0) {
 		config->default_layout = L_TABBED;
 	} else {
 		return cmd_results_new(CMD_INVALID,
-				"Expected 'workspace_layout <default|stacking|tabbed>'");
+				"Expected 'workspace_layout <default|splith|splitv|stacking|tabbed>'");
 	}
 	return cmd_results_new(CMD_SUCCESS, NULL);
 }
