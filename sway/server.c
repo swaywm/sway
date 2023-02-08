84abdd93d3ef72089340c483d3543ca70003ead3
Simon Ser <contact@emersion.fr>
2023-02-08 15:28:20 +0100

    Send wl_surface.preferred_buffer_scale
    References: https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/3696

--- sway/server.c
+++ sway/server.c
@@ -103,7 +103,7 @@ bool server_init(struct sway_server *server) {
 		return false;
 	}
 
-	server->compositor = wlr_compositor_create(server->wl_display, 5,
+	server->compositor = wlr_compositor_create(server->wl_display, 6,
 		server->renderer);
 	server->compositor_new_surface.notify = handle_compositor_new_surface;
 	wl_signal_add(&server->compositor->events.new_surface,
