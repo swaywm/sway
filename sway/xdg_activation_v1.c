swaywm/sway

commit 842609da6432c054c19c8d10f1660085653daa64

view: make request_activate take a seat
This way we can move focus on the same seat an activation token
originates from.

sway/xdg_activation_v1.c
@@ -31,5 +31,10 @@ void xdg_activation_v1_handle_request_activate(struct wl_listener *listener,
 		return;
 	}
 
-	view_request_activate(view);
+	struct wlr_seat *wlr_seat = event->token->seat;
+	// The requesting seat may have been destroyed.
+	if (wlr_seat) {
+		struct sway_seat *seat = wlr_seat->data;
+		view_request_activate(view, seat);
+	}
 }

include/sway/tree/view.h
@@ -271,7 +271,7 @@ void view_set_activated(struct sway_view *view, bool activated);
 /**
  * Called when the view requests to be focused.
  */
-void view_request_activate(struct sway_view *view);
+void view_request_activate(struct sway_view *view, struct sway_seat *seat);
 
 /**
  * If possible, instructs the client to change their decoration mode.

sway/desktop/xwayland.c
@@ -630,7 +630,7 @@ static void handle_request_activate(struct wl_listener *listener, void *data) {
 	if (!xsurface->mapped) {
 		return;
 	}
-	view_request_activate(view);
+	view_request_activate(view, NULL);
 
 	transaction_commit_dirty();
 }

sway/tree/view.c
@@ -366,12 +366,14 @@ void view_set_activated(struct sway_view *view, bool activated) {
 	}
 }
 
-void view_request_activate(struct sway_view *view) {
+void view_request_activate(struct sway_view *view, struct sway_seat *seat) {
 	struct sway_workspace *ws = view->container->pending.workspace;
 	if (!ws) { // hidden scratchpad container
 		return;
 	}
-	struct sway_seat *seat = input_manager_current_seat();
+	if (!seat) {
+		seat = input_manager_current_seat();
+	}
 
 	switch (config->focus_on_window_activation) {
 	case FOWA_SMART:

commit 1cb285c4c14869ecc113c92627b66ea24956cfa1

xdg-activation: fix urgency when the client does not specify a seat
xdg-activation is now too strict in only allowing tokens with a seat to
activate a surface. Clients may rely on this behavior for urgency hints.
The seat argument is still useful in case the client does provide a seat
so we can activate it on the desired seat.

Fixes: 842609da6432 (view: make request_activate take a seat, 2022-11-30)

sway/xdg_activation_v1.c
@@ -33,11 +33,8 @@ void xdg_activation_v1_handle_request_activate(struct wl_listener *listener,
 	}
 
 	struct wlr_seat *wlr_seat = event->token->seat;
-	// The requesting seat may have been destroyed.
-	if (wlr_seat) {
-		struct sway_seat *seat = wlr_seat->data;
-		view_request_activate(view, seat);
-	}
+	struct sway_seat *seat = wlr_seat ? wlr_seat->data : NULL;
+	view_request_activate(view, seat);
 }
 
 void xdg_activation_v1_handle_new_token(struct wl_listener *listener, void *data) {