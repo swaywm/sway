781ed74bf63526ad6976e626105ac0beee918531
Simon Ser <contact@emersion.fr>
2023-12-19 19:31:17 +0100

    Use strcasecmp, error out on '*'

--- sway/commands/output/adaptive_sync.c
+++ sway/commands/output/adaptive_sync.c
@@ -1,3 +1,4 @@
+#include <strings.h>
 #include "sway/commands.h"
 #include "sway/config.h"
 #include "sway/output.h"
@@ -13,14 +14,19 @@ struct cmd_results *output_cmd_adaptive_sync(int argc, char **argv) {
 
 	bool current_value = true;
 	
-	if (strcmp(argv[0], "toggle") == 0) {
-		struct output_config *oc = config->handler_context.output_config;
-		struct sway_output *sway_output = all_output_by_name_or_id(oc->name);
+	if (strcasecmp(argv[0], "toggle") == 0) {
+		const char *oc_name = config->handler_context.output_config->name;
+		if (strcmp(oc_name, "*") == 0) {
+			return cmd_results_new(CMD_INVALID,
+				"Cannot apply toggle to all outputs");
+		}
 
+		struct sway_output *sway_output = all_output_by_name_or_id(oc_name);
 		if (!sway_output || !sway_output->wlr_output) {
 			return cmd_results_new(CMD_FAILURE,
-				   "Cannot apply toggle to unknown output %s", oc->name);
+				"Cannot apply toggle to unknown output %s", oc_name);
 		}
+
 		current_value =
 			sway_output->wlr_output->adaptive_sync_status == WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED;
 	}
