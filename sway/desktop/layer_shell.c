swaywm/sway

commit 2c69e19fd30c5ceac61d6af821c6c6875cd3f8b6

layer-shell: don't configure uninitialized surfaces

sway/desktop/layer_shell.c
@@ -131,6 +131,9 @@ static void arrange_layer(struct sway_output *output, struct wl_list *list,
 			&full_area.width, &full_area.height);
 	wl_list_for_each(sway_layer, list, link) {
 		struct wlr_layer_surface_v1 *layer = sway_layer->layer_surface;
+		if (!layer->initialized) {
+			return;
+		}
 		struct wlr_layer_surface_v1_state *state = &layer->current;
 		if (exclusive != (state->exclusive_zone > 0)) {
 			continue;
@@ -327,6 +330,8 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 
 	if (layer_surface->initial_commit) {
 		surface_enter_output(layer_surface->surface, output);
+	} else if (!layer_surface->initialized) {
+		return;
 	}
 
 	struct wlr_box old_extent = layer->extent;

commit 3a942f565db24aff2af92751afcfd398551266f4

layer-shell: handle commits from uninitialized surfaces
Fixes: 2c69e19fd30c5ceac61d6af821c6c6875cd3f8b6

sway/desktop/layer_shell.c
@@ -205,9 +205,6 @@ static void handle_surface_commit(struct wl_listener *listener, void *data) {
 		wl_container_of(listener, surface, surface_commit);
 
 	struct wlr_layer_surface_v1 *layer_surface = surface->layer_surface;
-	if (!layer_surface->initialized) {
-		return;
-	}
 
 	uint32_t committed = layer_surface->current.committed;
 	if (committed & WLR_LAYER_SURFACE_V1_STATE_LAYER) {