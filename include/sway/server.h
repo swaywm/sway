1875e03dd062f8eda768336a8f82113b49183d7b
Simon Ser <contact@emersion.fr>
2024-02-15 11:52:38 +0100

    Fix build with wlroots DRM backend disabled
    The header is not installed by wlroots when the DRM backend is
    disabled. We don't need it here, so don't include it.
    
    Closes: https://github.com/swaywm/sway/issues/7943
    (cherry picked from commit ca40663d4277ade2a7dfec0484db5b7881799ee4)

--- include/sway/server.h
+++ include/sway/server.h
@@ -9,7 +9,6 @@
 #include <wlr/types/wlr_data_device.h>
 #include <wlr/types/wlr_input_method_v2.h>
 #include <wlr/types/wlr_foreign_toplevel_management_v1.h>
-#include <wlr/types/wlr_drm_lease_v1.h>
 #include <wlr/types/wlr_layer_shell_v1.h>
 #include <wlr/types/wlr_output_management_v1.h>
 #include <wlr/types/wlr_output_power_management_v1.h>
