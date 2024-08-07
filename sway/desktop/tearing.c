swaywm/sway

commit 9a1c411abd8261c121dcd50dfe54132718768084

Add support for tearing-control-v1
References: https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/3871

Adds option to allow tearing per output, as well as an option to force
enable or disable tearing for a specific application using a window
rule. Only works with fullscreen applications.

sway/desktop/tearing.c
@@ -1,0 +1,62 @@
+#include <wayland-server-core.h>
+#include <wlr/types/wlr_tearing_control_v1.h>
+#include "sway/server.h"
+#include "sway/tree/view.h"
+#include "sway/output.h"
+#include "log.h"
+
+struct sway_tearing_controller {
+	struct wlr_tearing_control_v1 *tearing_control;
+	struct wl_listener set_hint;
+	struct wl_listener destroy;
+
+	struct wl_list link; // sway_server::tearing_controllers
+};
+
+static void handle_tearing_controller_set_hint(struct wl_listener *listener,
+		void *data) {
+	struct sway_tearing_controller *controller =
+		wl_container_of(listener, controller, set_hint);
+	
+	struct sway_view *view = view_from_wlr_surface(
+		controller->tearing_control->surface);
+	if (view) {
+		view->tearing_hint = controller->tearing_control->current;
+	}
+}
+
+static void handle_tearing_controller_destroy(struct wl_listener *listener, 
+		void *data) {
+	struct sway_tearing_controller *controller = 
+		wl_container_of(listener, controller, destroy);
+	wl_list_remove(&controller->link);
+	free(controller);
+}
+
+void handle_new_tearing_hint(struct wl_listener *listener, 
+		void *data) {
+	struct sway_server *server = 
+		wl_container_of(listener, server, tearing_control_new_object);
+	struct wlr_tearing_control_v1 *tearing_control = data;
+	
+	enum wp_tearing_control_v1_presentation_hint hint = 
+		wlr_tearing_control_manager_v1_surface_hint_from_surface(
+			server->tearing_control_v1, tearing_control->surface);
+	sway_log(SWAY_DEBUG, "New presentation hint %d received for surface %p",
+		hint, tearing_control->surface);
+
+	struct sway_tearing_controller *controller = 
+		calloc(1, sizeof(struct sway_tearing_controller));
+	if (!controller) {
+		return;
+	}
+
+	controller->tearing_control = tearing_control;
+	controller->set_hint.notify = handle_tearing_controller_set_hint;
+	wl_signal_add(&tearing_control->events.set_hint, &controller->set_hint);
+	controller->destroy.notify = handle_tearing_controller_destroy;
+	wl_signal_add(&tearing_control->events.destroy, &controller->destroy);
+	wl_list_init(&controller->link);
+
+	wl_list_insert(&server->tearing_controllers, &controller->link);
+}

include/sway/commands.h
@@ -104,6 +104,7 @@ struct sway_container *container_find_resize_parent(struct sway_container *con,
 sway_cmd cmd_exec_validate;
 sway_cmd cmd_exec_process;
 
+sway_cmd cmd_allow_tearing;
 sway_cmd cmd_assign;
 sway_cmd cmd_bar;
 sway_cmd cmd_bindcode;
@@ -283,6 +284,7 @@ sway_cmd input_cmd_xkb_switch_layout;
 sway_cmd input_cmd_xkb_variant;
 
 sway_cmd output_cmd_adaptive_sync;
+sway_cmd output_cmd_allow_tearing;
 sway_cmd output_cmd_background;
 sway_cmd output_cmd_color_profile;
 sway_cmd output_cmd_disable;

include/sway/config.h
@@ -289,6 +289,7 @@ struct output_config {
 	enum render_bit_depth render_bit_depth;
 	bool set_color_transform;
 	struct wlr_color_transform *color_transform;
+	int allow_tearing;
 
 	char *background;
 	char *background_option;

include/sway/output.h
@@ -73,6 +73,7 @@ struct sway_output {
 	int max_render_time; // In milliseconds
 	struct wl_event_source *repaint_timer;
 	bool gamma_lut_changed;
+	bool allow_tearing;
 };
 
 struct sway_output_non_desktop {

include/sway/server.h
@@ -115,6 +115,10 @@ struct sway_server {
 	struct wl_listener xdg_activation_v1_new_token;
 
 	struct wl_listener request_set_cursor_shape;
+	
+	struct wlr_tearing_control_manager_v1 *tearing_control_v1;
+	struct wl_listener tearing_control_new_object;
+	struct wl_list tearing_controllers; // sway_tearing_controller::link
 
 	struct wl_list pending_launcher_ctxs; // launcher_ctx::link
 
@@ -181,5 +185,7 @@ void xdg_activation_v1_handle_new_token(struct wl_listener *listener,
 	void *data);
 
 void set_rr_scheduling(void);
+
+void handle_new_tearing_hint(struct wl_listener *listener, void *data);
 
 #endif

include/sway/tree/view.h
@@ -4,6 +4,7 @@
 #include <wlr/config.h>
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_tearing_control_v1.h>
 #include "sway/config.h"
 #if WLR_HAS_XWAYLAND
 #include <wlr/xwayland.h>
@@ -32,6 +33,12 @@ enum sway_view_prop {
 	VIEW_PROP_X11_WINDOW_ID,
 	VIEW_PROP_X11_PARENT_ID,
 #endif
+};
+
+enum sway_view_tearing_mode {
+	TEARING_OVERRIDE_FALSE,
+	TEARING_OVERRIDE_TRUE,
+	TEARING_WINDOW_HINT,
 };
 
 struct sway_view_impl {
@@ -111,6 +118,9 @@ struct sway_view {
 	int max_render_time; // In milliseconds
 
 	enum seat_config_shortcuts_inhibit shortcuts_inhibit;
+
+	enum sway_view_tearing_mode tearing_mode;
+	enum wp_tearing_control_v1_presentation_hint tearing_hint;
 };
 
 struct sway_xdg_shell_view {
@@ -334,5 +344,7 @@ bool view_is_transient_for(struct sway_view *child, struct sway_view *ancestor);
 void view_assign_ctx(struct sway_view *view, struct launcher_ctx *ctx);
 
 void view_send_frame_done(struct sway_view *view);
+
+bool view_can_tear(struct sway_view *view);
 
 #endif

protocols/meson.build
@@ -14,6 +14,7 @@ protocols = [
 	wl_protocol_dir / 'unstable/linux-dmabuf/linux-dmabuf-unstable-v1.xml',
 	wl_protocol_dir / 'staging/content-type/content-type-v1.xml',
 	wl_protocol_dir / 'staging/cursor-shape/cursor-shape-v1.xml',
+	wl_protocol_dir / 'staging/tearing-control/tearing-control-v1.xml',
 	'wlr-layer-shell-unstable-v1.xml',
 	'idle.xml',
 	'wlr-output-power-management-unstable-v1.xml',

sway/commands.c
@@ -112,6 +112,7 @@ static const struct cmd_handler config_handlers[] = {
 
 /* Runtime-only commands. Keep alphabetized */
 static const struct cmd_handler command_handlers[] = {
+	{ "allow_tearing", cmd_allow_tearing },
 	{ "border", cmd_border },
 	{ "create_output", cmd_create_output },
 	{ "exit", cmd_exit },

commit f69041e620304277bb05e07d24aa6e108c10ab88

tearing: fix UAF on destroy
Fixes: 9a1c411abd8261c121dcd50dfe54132718768084

sway/desktop/tearing.c
@@ -29,6 +29,8 @@ static void handle_tearing_controller_destroy(struct wl_listener *listener,
 		void *data) {
 	struct sway_tearing_controller *controller =
 		wl_container_of(listener, controller, destroy);
+	wl_list_remove(&controller->set_hint.link);
+	wl_list_remove(&controller->destroy.link);
 	wl_list_remove(&controller->link);
 	free(controller);
 }