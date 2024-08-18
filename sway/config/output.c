swaywm/sway

commit 4e38f93f367dfb7f1ec66060e6262b806cecf3a7

config/output: Skip VRR tests when not supported
Adaptive sync is a "soft" setting which we degrade of off when not
supported. Some outputs types do not support turning it off (Wayland,
X11), which makes for an awkward three-way test where we first enable,
disable and finally unset the setting.

wlr_output.adaptive_sync_supported tells us whether the output
definitely does not support making changes (backend without support,
connector without the feature), or whether it might work.

Use this to avoid wasting time on adaptive sync test that can never
succeed, and to avoid the Wayland/X11-backend specific unset step.

sway/config/output.c
@@ -721,21 +721,18 @@ static bool search_adaptive_sync(struct search_context *ctx, size_t output_idx) 
 	struct wlr_backend_output_state *backend_state = &ctx->states[output_idx];
 	struct wlr_output_state *state = &backend_state->base;
 
+	if (!backend_state->output->adaptive_sync_supported) {
+		return search_finish(ctx, output_idx);
+	}
+
 	if (cfg->config && cfg->config->adaptive_sync == 1) {
 		wlr_output_state_set_adaptive_sync_enabled(state, true);
 		if (search_finish(ctx, output_idx)) {
 			return true;
 		}
 	}
-	if (!cfg->config || cfg->config->adaptive_sync != -1) {
-		wlr_output_state_set_adaptive_sync_enabled(state, false);
-		if (search_finish(ctx, output_idx)) {
-			return true;
-		}
-	}
-	// If adaptive sync has not been set, or fallback in case we are on a
-	// backend that cannot disable adaptive sync such as the wayland backend.
-	state->committed &= ~WLR_OUTPUT_STATE_ADAPTIVE_SYNC_ENABLED;
+
+	wlr_output_state_set_adaptive_sync_enabled(state, false);
 	return search_finish(ctx, output_idx);
 }
 

commit 1c7358149f8fd8623a73b440a9befa9c7f132842

config/output: Do not set adaptive_sync if not supported
After 4e38f93f367d ("config/output: Skip VRR tests when not supported"),
the configuration search no longer touches VRR state for outputs that
are known to not support it. This also means that it will not remove VRR
if already set, which could cause output configuration to fail.

Ensure that VRR state is never set for outputs that do not support it by
adding the same test for support to queue_output_config.

Fixes: 4e38f93f367d ("config/output: Skip VRR tests when not supported")
Fixes: https://github.com/swaywm/sway/issues/8296

sway/config/output.c
@@ -491,7 +491,7 @@ static void queue_output_config(struct output_config *oc,
 		wlr_output_state_set_scale(pending, scale);
 	}
 
-	if (oc && oc->adaptive_sync != -1) {
+	if (oc && oc->adaptive_sync != -1 && wlr_output->adaptive_sync_supported) {
 		sway_log(SWAY_DEBUG, "Set %s adaptive sync to %d", wlr_output->name,
 			oc->adaptive_sync);
 		wlr_output_state_set_adaptive_sync_enabled(pending, oc->adaptive_sync == 1);