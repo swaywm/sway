535f0ee098fb466c247ddccabdd5d68b51c29d17
Kenny Levinsen <kl@kl.wtf>
2024-09-06 00:35:57 +0200

    tree/arrange: Remove redundant output geometry update
    This is handled by apply_output_configs.
    
    (cherry picked from 4f9ce4675cf428e8acd632de31482981e1bedcf8)

--- sway/tree/arrange.c
+++ sway/tree/arrange.c
@@ -314,14 +314,6 @@ void arrange_output(struct sway_output *output) {
 	if (config->reloading) {
 		return;
 	}
-	struct wlr_box output_box;
-	wlr_output_layout_get_box(root->output_layout,
-		output->wlr_output, &output_box);
-	output->lx = output_box.x;
-	output->ly = output_box.y;
-	output->width = output_box.width;
-	output->height = output_box.height;
-
 	for (int i = 0; i < output->workspaces->length; ++i) {
 		struct sway_workspace *workspace = output->workspaces->items[i];
 		arrange_workspace(workspace);
