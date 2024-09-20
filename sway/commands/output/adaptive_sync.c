4cca00458341e42c6241149f149a76eafe868b1a
Emil Engberg <emil.engberg@outlook.com>
2024-09-20 15:36:58 +0200

    Add toggle for output adaptive_sync
    (cherry picked from commit e940acd3749a5af08d5c404cae242c8693784ddc)

--- sway/commands/output/adaptive_sync.c
+++ sway/commands/output/adaptive_sync.c
@@ -1,5 +1,7 @@
+#include <strings.h>
 #include "sway/commands.h"
 #include "sway/config.h"
+#include "sway/output.h"
 #include "util.h"
 
 struct cmd_results *output_cmd_adaptive_sync(int argc, char **argv) {
@@ -10,11 +12,25 @@ struct cmd_results *output_cmd_adaptive_sync(int argc, char **argv) {
 		return cmd_results_new(CMD_INVALID, "Missing adaptive_sync argument");
 	}
 
-	if (parse_boolean(argv[0], true)) {
-		config->handler_context.output_config->adaptive_sync = 1;
-	} else {
-		config->handler_context.output_config->adaptive_sync = 0;
+	bool current_value = true;
+	if (strcasecmp(argv[0], "toggle") == 0) {
+		const char *oc_name = config->handler_context.output_config->name;
+		if (strcmp(oc_name, "*") == 0) {
+			return cmd_results_new(CMD_INVALID,
+				"Cannot apply toggle to all outputs");
+		}
+
+		struct sway_output *sway_output = all_output_by_name_or_id(oc_name);
+		if (!sway_output || !sway_output->wlr_output) {
+			return cmd_results_new(CMD_FAILURE,
+				"Cannot apply toggle to unknown output %s", oc_name);
+		}
+
+		current_value =
+			sway_output->wlr_output->adaptive_sync_status == WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED;
 	}
+
+	config->handler_context.output_config->adaptive_sync = parse_boolean(argv[0], current_value);
 
 	config->handler_context.leftovers.argc = argc - 1;
 	config->handler_context.leftovers.argv = argv + 1;
