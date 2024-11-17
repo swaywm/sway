swaywm/sway

commit fdc4318ac66d257d21e8f3b953e341d5e80a1ddc

desktop/output: Clear frame_pending even output is disabled
frame_pending should always be cleared once the repaint callback is
fired to ensure that future frame scheduling is not accidentally held
back.

sway/desktop/output.c
@@ -262,11 +262,10 @@ static bool output_can_tear(struct sway_output *output) {
 static int output_repaint_timer_handler(void *data) {
 	struct sway_output *output = data;
 
+	output->wlr_output->frame_pending = false;
 	if (!output->enabled) {
 		return 0;
 	}
-
-	output->wlr_output->frame_pending = false;
 
 	output_configure_scene(output, &root->root_scene->tree.node, 1.0f);
 

commit b63d9fd91d347a572ab9db086212e7fff1932a8b

desktop/output: Clear repaint timer earlier in destroy
The teardown of a sway_output is split in two: begin_destroy and
output_destroy. The former clears some state such as NULL'ing the
reference to wlr_output, while the latter frees the struct and its
remaining resources.

If an output is destroyed while a repaint timer is pending, future frame
callbacks will no longer occur as the listener is torn down in
begin_destroy, but the repaint timer is not torn down and may still
fire until output_destroy is hit. As begin_destroy cleared the reference
to wlr_output, this leads to a NULL-pointer dereference.

Tear down the repaint timer in begin_destroy as there is no need for it.

Fixes: fdc4318ac66d ("desktop/output: Clear frame_pending even output is disabled")

sway/desktop/output.c
@@ -440,6 +440,9 @@ static void begin_destroy(struct sway_output *output) {
 	output->wlr_output->data = NULL;
 	output->wlr_output = NULL;
 
+	wl_event_source_remove(output->repaint_timer);
+	output->repaint_timer = NULL;
+
 	request_modeset();
 }
 

sway/tree/output.c
@@ -273,7 +273,6 @@ void output_destroy(struct sway_output *output) {
 	destroy_scene_layers(output);
 	list_free(output->workspaces);
 	list_free(output->current.workspaces);
-	wl_event_source_remove(output->repaint_timer);
 	wlr_color_transform_unref(output->color_transform);
 	free(output);
 }