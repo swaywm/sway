swaywm/sway

commit 0c60d1581f7b12ae472c786b7dfe27a1c6ec9a47

Use has_prefix() instead of strncmp() throughout
This is safer than hardcoded string lengths.

sway/commands/bind.c
@@ -367,8 +367,7 @@ static struct cmd_results *cmd_bindsym_or_bindcode(int argc, char **argv,
 			}
 		} else if (strcmp("--exclude-titlebar", argv[0]) == 0) {
 			exclude_titlebar = true;
-		} else if (strncmp("--input-device=", argv[0],
-					strlen("--input-device=")) == 0) {
+		} else if (has_prefix("--input-device=", argv[0])) {
 			free(binding->input);
 			binding->input = strdup(argv[0] + strlen("--input-device="));
 			strip_quotes(binding->input);
@@ -399,7 +398,7 @@ static struct cmd_results *cmd_bindsym_or_bindcode(int argc, char **argv,
 	list_t *split = split_string(argv[0], "+");
 	for (int i = 0; i < split->length; ++i) {
 		// Check for group
-		if (strncmp(split->items[i], "Group", strlen("Group")) == 0) {
+		if (has_prefix(split->items[i], "Group") == 0) {
 			if (binding->group != XKB_LAYOUT_INVALID) {
 				free_sway_binding(binding);
 				list_free_items_and_destroy(split);

sway/commands/mark.c
@@ -23,7 +23,7 @@ struct cmd_results *cmd_mark(int argc, char **argv) {
 	}
 
 	bool add = false, toggle = false;
-	while (argc > 0 && strncmp(*argv, "--", 2) == 0) {
+	while (argc > 0 && has_prefix(*argv, "--") == 0) {
 		if (strcmp(*argv, "--add") == 0) {
 			add = true;
 		} else if (strcmp(*argv, "--replace") == 0) {

sway/commands/assign.c
@@ -23,7 +23,7 @@ struct cmd_results *cmd_assign(int argc, char **argv) {
 
 	--argc; ++argv;
 
-	if (strncmp(*argv, "→", strlen("→")) == 0) {
+	if (has_prefix(*argv, "→")) {
 		if (argc < 2) {
 			free(criteria);
 			return cmd_results_new(CMD_INVALID, "Missing workspace");

sway/commands/bar/font.c
@@ -11,7 +11,7 @@ struct cmd_results *bar_cmd_font(int argc, char **argv) {
 	char *font = join_args(argv, argc);
 	free(config->current_bar->font);
 
-	if (strncmp(font, "pango:", 6) == 0) {
+	if (has_prefix(font, "pango:")) {
 		if (config->current_bar->pango_markup == PANGO_MARKUP_DEFAULT) {
 			config->current_bar->pango_markup = true;
 		}

sway/commands/font.c
@@ -13,9 +13,9 @@ struct cmd_results *cmd_font(int argc, char **argv) {
 	char *font = join_args(argv, argc);
 	free(config->font);
 
-	if (strncmp(font, "pango:", 6) == 0) {
+	if (has_prefix(font, "pango:")) {
 		config->pango_markup = true;
-		config->font = strdup(font + 6);
+		config->font = strdup(font + strlen("pango:"));
 		free(font);
 	} else {
 		config->pango_markup = false;

sway/commands/gesture.c
@@ -121,8 +121,7 @@ static struct cmd_results *cmd_bind_or_unbind_gesture(int argc, char **argv, boo
 			binding->flags |= BINDING_EXACT;
 		} else if (strcmp("--no-warn", argv[0]) == 0) {
 			warn = false;
-		} else if (strncmp("--input-device=", argv[0],
-					strlen("--input-device=")) == 0) {
+		} else if (has_prefix("--input-device=", argv[0])) {
 			free(binding->input);
 			binding->input = strdup(argv[0] + strlen("--input-device="));
 		} else {

sway/commands/input/events.c
@@ -86,7 +86,7 @@ static void toggle_select_send_events_for_device(struct input_config *ic,
 static void toggle_send_events(int argc, char **argv) {
 	struct input_config *ic = config->handler_context.input_config;
 	bool wildcard = strcmp(ic->identifier, "*") == 0;
-	const char *type = strncmp(ic->identifier, "type:", strlen("type:")) == 0
+	const char *type = has_prefix(ic->identifier, "type:")
 		? ic->identifier + strlen("type:") : NULL;
 	struct sway_input_device *device = NULL;
 	wl_list_for_each(device, &server.input->devices, link) {
@@ -146,8 +146,7 @@ struct cmd_results *input_cmd_events(int argc, char **argv) {
 
 		toggle_send_events(argc - 1, argv + 1);
 
-		if (strcmp(ic->identifier, "*") == 0 ||
-				strncmp(ic->identifier, "type:", strlen("type:")) == 0) {
+		if (strcmp(ic->identifier, "*") == 0 || has_prefix(ic->identifier, "type:")) {
 			// Update the device input configs and then reset the type/wildcard
 			// config send events mode so that is does not override the device
 			// ones. The device ones will be applied when attempting to apply

sway/config.c
@@ -925,8 +925,8 @@ char *do_var_replacement(char *str) {
 		// Find matching variable
 		for (i = 0; i < config->symbols->length; ++i) {
 			struct sway_variable *var = config->symbols->items[i];
-			int vnlen = strlen(var->name);
-			if (strncmp(find, var->name, vnlen) == 0) {
+			if (has_prefix(find, var->name)) {
+				int vnlen = strlen(var->name);
 				int vvlen = strlen(var->value);
 				char *newstr = malloc(strlen(str) - vnlen + vvlen + 1);
 				if (!newstr) {

commit a2bb9e4079c1d996f3fa2ae2260b5d0e506c1c2f

Fix has_prefix() comparisons with 0
has_prefix() returns a bool, unlike strncmp() which returns an int.

Fixes: 0c60d1581f7b ("Use has_prefix() instead of strncmp() throughout")
Closes: https://github.com/swaywm/sway/issues/8527

sway/commands/bind.c
@@ -398,7 +398,7 @@ static struct cmd_results *cmd_bindsym_or_bindcode(int argc, char **argv,
 	list_t *split = split_string(argv[0], "+");
 	for (int i = 0; i < split->length; ++i) {
 		// Check for group
-		if (has_prefix(split->items[i], "Group") == 0) {
+		if (has_prefix(split->items[i], "Group")) {
 			if (binding->group != XKB_LAYOUT_INVALID) {
 				free_sway_binding(binding);
 				list_free_items_and_destroy(split);

sway/commands/mark.c
@@ -23,7 +23,7 @@ struct cmd_results *cmd_mark(int argc, char **argv) {
 	}
 
 	bool add = false, toggle = false;
-	while (argc > 0 && has_prefix(*argv, "--") == 0) {
+	while (argc > 0 && has_prefix(*argv, "--")) {
 		if (strcmp(*argv, "--add") == 0) {
 			add = true;
 		} else if (strcmp(*argv, "--replace") == 0) {