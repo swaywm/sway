swaywm/sway

commit 5e18ed3cf03eee9e83909fede46dd98dff652647

commands/move: do not force focus on the moved container
My code archaeology isn't good enough to determine what this is here
for, but it isn't correct. We should be able to move containers in a
direction without focusing them. AFAICT i3 doesn't do this, so we
shouldn't either.

This fixes ipc commands like move <dir> with criteria that apply to
containers which are not the current focus.

sway/commands/move.c
@@ -769,15 +769,6 @@ static struct cmd_results *cmd_move_in_direction(
 		ipc_event_window(container, "move");
 	}
 
-	// Hack to re-focus container
-	seat_set_raw_focus(config->handler_context.seat, &new_ws->node);
-	seat_set_focus_container(config->handler_context.seat, container);
-
-	if (old_ws != new_ws) {
-		ipc_event_workspace(old_ws, new_ws, "focus");
-		workspace_detect_urgent(old_ws);
-		workspace_detect_urgent(new_ws);
-	}
 	container_end_mouse_operation(container);
 
 	return cmd_results_new(CMD_SUCCESS, NULL);

commit f0731de2d38e36e61b6cbd1cde88a7a5c8c01d90

Properly focus container when it changes parent
When a parented container changes parent, sway will render a sibling
non-focused container.

Revert deletion of code which introduced this regression.

Fixes: 5e18ed3cf03eee9e83909fede46dd98dff652647
Fixes: https://github.com/swaywm/sway/issues/8292

sway/commands/move.c
@@ -770,6 +770,10 @@ static struct cmd_results *cmd_move_in_direction(
 		ipc_event_window(container, "move");
 	}
 
+	// Re-focus re-parented container.
+	seat_set_raw_focus(config->handler_context.seat, &new_ws->node);
+	seat_set_focus_container(config->handler_context.seat, container);
+
 	container_end_mouse_operation(container);
 
 	return cmd_results_new(CMD_SUCCESS, NULL);