381c01a622b1c65a8428eb7c4b6967590d4d0d07
Konstantin Pospelov <kupospelov@gmail.com>
2025-03-29 22:48:46 +0100

    swaybar: separate adjacent borders
    This commit decouples the surface height from the usable height, making it
    possible to draw a separator between the workspace/mode and window borders
    on top (for bottom bars) or bottom (for top bars). The workspace and mode
    borders also get separated.
    
    This behavior is consistent with i3bar.

--- include/swaybar/tray/tray.h
+++ include/swaybar/tray/tray.h
@@ -37,6 +37,7 @@ struct swaybar_tray {
 struct swaybar_tray *create_tray(struct swaybar *bar);
 void destroy_tray(struct swaybar_tray *tray);
 void tray_in(int fd, short mask, void *data);
-uint32_t render_tray(cairo_t *cairo, struct swaybar_output *output, double *x);
+uint32_t render_tray(cairo_t *cairo, struct swaybar_output *output,
+		double *x, uint32_t height);
 
 #endif
