14df3966f1d15d985ab40e5ebff058c8510a436c
Simon Ser <contact@emersion.fr>
2025-05-06 09:20:46 +0200

    Drop sway_output.events.disable
    In general wl_signal isn't well-suited for Sway: Sway doesn't need
    any modularity, and signals make it trickier to track down exactly
    what happens down the stack.
    
    Replace Sway's output disable signal with a simple list tracking
    for the only user.

--- include/sway/output.h
+++ include/sway/output.h
@@ -52,6 +52,7 @@ struct sway_output {
 
 	bool enabled;
 	list_t *workspaces;
+	struct wl_list layer_surfaces; // sway_layer_surface.link
 
 	struct sway_output_state current;
 
@@ -60,10 +61,6 @@ struct sway_output {
 	struct wl_listener present;
 	struct wl_listener frame;
 	struct wl_listener request_state;
-
-	struct {
-		struct wl_signal disable;
-	} events;
 
 	struct wlr_color_transform *color_transform;
 
