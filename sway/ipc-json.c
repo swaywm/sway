ced728a1799b8f2a1b9ebcf5b64a855a2f8fe06f
Simon Ser <contact@emersion.fr>
2025-07-08 18:52:29 +0200

    Add features.hdr to output IPC response

--- sway/ipc-json.c
+++ sway/ipc-json.c
@@ -323,6 +323,8 @@ static void ipc_json_describe_wlr_output(struct wlr_output *wlr_output, json_obj
 	json_object_object_add(features_object, "adaptive_sync",
 		json_object_new_boolean(wlr_output->adaptive_sync_supported ||
 			wlr_output->adaptive_sync_status ==  WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED));
+	json_object_object_add(features_object, "hdr",
+		json_object_new_boolean(output_supports_hdr(wlr_output, NULL)));
 	json_object_object_add(object, "features", features_object);
 }
 
