swaywm/sway

commit f50e307227c8938a57c098edd77098786ea6613a

sway/commands/layout: flatten parent once
Applying layout changes to the parent of the parent, in case the parent only has a single child, stops the creation of a chain of single child containers.

Closes: https://github.com/swaywm/sway/issues/7945

sway/commands/layout.c
@@ -134,6 +134,15 @@ struct cmd_results *cmd_layout(int argc, char **argv) {
 	// Operate on parent container, like i3.
 	if (container) {
 		container = container->pending.parent;
+		// If parent has only a singe child operate on its parent and
+		// flatten once, like i3
+		if (container && container->pending.children->length == 1) {
+			struct sway_container *child = container->pending.children->items[0];
+			struct sway_container *parent = container->pending.parent;
+			container_replace(container, child);
+			container_begin_destroy(container);
+			container = parent;
+		}
 	}
 
 	// We could be working with a container OR a workspace. These are different

commit b7eb6177e1d634620d73d56224cf154e3b854e0f

sway/commands/layout: fix flatten parent once
Fixes: f50e307227c8 ("sway/commands/layout: flatten parent once")

sway/commands/layout.c
@@ -137,11 +137,15 @@ struct cmd_results *cmd_layout(int argc, char **argv) {
 		// If parent has only a singe child operate on its parent and
 		// flatten once, like i3
 		if (container && container->pending.children->length == 1) {
-			struct sway_container *child = container->pending.children->items[0];
+			// Also check grandparent to avoid restricting layouts
 			struct sway_container *parent = container->pending.parent;
-			container_replace(container, child);
-			container_begin_destroy(container);
-			container = parent;
+			if (parent && parent->pending.children->length == 1) {
+				struct sway_container *child = container->pending.children->items[0];
+				struct sway_container *parent = container->pending.parent;
+				container_replace(container, child);
+				container_begin_destroy(container);
+				container = parent;
+			}
 		}
 	}
 