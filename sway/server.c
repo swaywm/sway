d735bb99a9e97619e633257ea001b42e2bfe95a5
llyyr <llyyr.public@gmail.com>
2025-11-13 21:21:09 +0530

    sway/server: advertise wlr_color_representation_manager_v1

--- sway/server.c
+++ sway/server.c
@@ -11,6 +11,7 @@
 #include <wlr/render/wlr_renderer.h>
 #include <wlr/types/wlr_alpha_modifier_v1.h>
 #include <wlr/types/wlr_color_management_v1.h>
+#include <wlr/types/wlr_color_representation_v1.h>
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_content_type_v1.h>
 #include <wlr/types/wlr_cursor_shape_v1.h>
@@ -484,6 +485,9 @@ bool server_init(struct sway_server *server) {
 		});
 		wlr_scene_set_color_manager_v1(root->root_scene, cm);
 	}
+
+	wlr_color_representation_manager_v1_create_with_renderer(
+		server->wl_display, 1, server->renderer);
 
 	wl_list_init(&server->pending_launcher_ctxs);
 
