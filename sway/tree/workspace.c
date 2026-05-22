swaywm/sway

commit 92891fb1edef5136ae4eb35fec5b8523f031be81

commands/move: unwrap workspace container on move to new workspace
If moving e.g. `T[app app]` into a new workspace with `workspace_layout
tabbed`, then post-move the tree in that workspace will be `T[T[app
app]]`. This still happens with horizontal or vertical workspace layout,
but is less visible since those containers have no decorations.

Fixes #5426.

sway/tree/workspace.c
@@ -641,6 +641,21 @@ struct sway_container *workspace_wrap_children(struct sway_workspace *ws) {
 	return middle;
 }
 
+void workspace_unwrap_children(struct sway_workspace *ws,
+		struct sway_container *wrap) {
+	if (!sway_assert(workspace_is_empty(ws),
+			"target workspace must be empty")) {
+		return;
+	}
+
+	ws->layout = wrap->layout;
+	while (wrap->children->length) {
+		struct sway_container *child = wrap->children->items[0];
+		container_detach(child);
+		workspace_add_tiling(ws, child);
+	}
+}
+
 void workspace_detach(struct sway_workspace *workspace) {
 	struct sway_output *output = workspace->output;
 	int index = list_find(output->workspaces, workspace);

include/sway/tree/workspace.h
@@ -105,6 +105,9 @@ struct sway_container *workspace_find_container(struct sway_workspace *ws,
  */
 struct sway_container *workspace_wrap_children(struct sway_workspace *ws);
 
+void workspace_unwrap_children(struct sway_workspace *ws,
+		struct sway_container *wrap);
+
 void workspace_detach(struct sway_workspace *workspace);
 
 void workspace_add_tiling(struct sway_workspace *workspace,

sway/commands/move.c
@@ -208,9 +208,13 @@ static void container_move_to_workspace(struct sway_container *container,
 		}
 	} else {
 		container_detach(container);
-		container->width = container->height = 0;
-		container->width_fraction = container->height_fraction = 0;
-		workspace_add_tiling(workspace, container);
+		if (workspace_is_empty(workspace) && container->children) {
+			workspace_unwrap_children(workspace, container);
+		} else {
+			container->width = container->height = 0;
+			container->width_fraction = container->height_fraction = 0;
+			workspace_add_tiling(workspace, container);
+		}
 		container_update_representation(container);
 	}
 	if (container->view) {

commit 93c777a947b71b0a22936d2c0023152004e538f6

tree/workspace: fix unwrapping children to avoid redundant split
Before this change, with `workspace_layout tabbed`, moving T[app app] to
an empty workspace resulted in T[T[app] T[app]], as each child was
individually wrapped in a new split container.

It looks like commit 92891fb1 introduced workspace_unwrap_children to
fix an issue where moving a tabbed/stacked container to a new workspace
with workspace_layout tabbed would incorrectly nest the container,
creating T[T[app app]].

However, workspace_unwrap_children used workspace_add_tiling to add the
detached children to the workspace. Since commit ece6a1d4,
workspace_add_tiling checks for a configured default_layout and if it
finds one, calls container_split for every added window.

This commit changes the unwrapping logic to use
workspace_insert_tiling_direct instead of workspace_add_tiling,
avoiding the default_layout splitting behaviour.

sway/tree/workspace.c
@@ -920,7 +920,7 @@ void workspace_unwrap_children(struct sway_workspace *ws,
 	while (wrap->pending.children->length) {
 		struct sway_container *child = wrap->pending.children->items[0];
 		container_detach(child);
-		workspace_add_tiling(ws, child);
+		workspace_insert_tiling_direct(ws, child, ws->tiling->length);
 	}
 }
 